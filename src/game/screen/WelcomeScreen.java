75d47503615cfef8d8b7daf26bb74ac6d84bf76c
lkumtamu <lkumtamu@asu.edu>
2023-10-27 23:34:15 -0700

    Added title and animation in welcome screen

---
+++ src/game/screen/WelcomeScreen.java
@@ -0,0 +1,53 @@
+package game.screen;
+
+import game.entity.Entity;
+import game.entity.Character;
+import game.entity.Text;
+import game.event.KeyHandler;
+import game.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.function.Consumer;
+
+public class WelcomeScreen extends Screen {
+    public WelcomeScreen(GamePanel gp, KeyHandler kh) {
+        super(gp, kh);
+
+        Text title = new Text("Welcome Screen", gp, kh);
+        int screenWidth = gp.getWidth();
+        int screenHeight = gp.getHeight();
+
+
+        title.x = (screenWidth) / 2;
+        title.y = 100;
+
+        Character genie = new Character(gp, kh);
+        genie.x = (screenWidth - 525) / 2;
+        ;
+        genie.y = (screenHeight - 275) / 2;
+
+        entities.add(title);
+        entities.add(genie);
+    }
+
+
+    @Override
+    public void draw(Graphics2D g) {
+        entities.forEach(new Consumer<Entity>() {
+            @Override
+            public void accept(Entity entity) {
+                entity.draw(g);
+            }
+        });
+    }
+    @Override
+    public void update() {
+        entities.forEach(new Consumer<Entity>() {
+            @Override
+            public void accept(Entity entity) {
+                entity.update();
+            }
+        });
+    }
+}
