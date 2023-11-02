04409c55fcb3e3d240038ffeff835b09d199f560
lkumtamu <lkumtamu@asu.edu>
2023-11-01 22:45:04 -0700

    TG-51 added sprite parser for getting image path for character animation

--- src/game/screen/WelcomeScreen.java
+++ src/game/screen/WelcomeScreen.java
@@ -3,32 +3,36 @@ package game.screen;
 import game.entity.Entity;
 import game.entity.Character;
 import game.entity.Text;
+import game.entity.TextInputBox;
 import game.event.KeyHandler;
 import game.GamePanel;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.function.Consumer;
 
 public class WelcomeScreen extends Screen {
+    private JTextField usernameInputField;
+
     public WelcomeScreen(GamePanel gp, KeyHandler kh) {
         super(gp, kh);
 
         Text title = new Text("Welcome Screen", gp, kh);
         int screenWidth = gp.getWidth();
         int screenHeight = gp.getHeight();
-
-
         title.x = (screenWidth) / 2;
         title.y = 100;
 
         Character genie = new Character(gp, kh);
         genie.x = (screenWidth - 525) / 2;
-        ;
         genie.y = (screenHeight - 275) / 2;
+
+        TextInputBox textInputBox = new TextInputBox(gp,kh);
 
         entities.add(title);
         entities.add(genie);
+        entities.add(textInputBox);
     }
 
 
