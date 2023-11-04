3cba2bdcdbe1b2a1fbb24bc46d016e7616ed013d
Harika Arimilli <harimill@asu.edu>
2023-11-03 22:35:31 -0700

    TG-52 integrated spriteparser to fetch character sprite images

--- src/game/entity/Character.java
+++ src/game/entity/Character.java
@@ -10,19 +10,13 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
 
-public class Character extends Entity {
+public class Character extends Entity{
 
     int spriteMax = 0;
+    int spriteCounterIncrement = 1;
 
-    public Character(GamePanel gp, KeyHandler kh, int x, int y, String spritePath) {
-        super(gp, kh);
-        this.x = x;
-        this.y = y;
-        atk = new BufferedImage[10];
-        getAttackImgs(spritePath);
-    }
 
-    public Character(GamePanel gp, KeyHandler kh, int x, int y, int width, int height, String spritePath) {
+    public Character(GamePanel gp, KeyHandler kh, int x, int y, int width, int height,String spritePath) {
         super(gp, kh);
         this.x = x;
         this.y = y;
@@ -34,47 +28,45 @@ public class Character extends Entity {
     @Override
     public void update() {
 
-        spriteCounter += 1;
-        if (spriteCounter > spriteMax) {
+        spriteCounter+=spriteCounterIncrement;
+        if(spriteCounter > spriteMax){
             spriteIndex += 1;
-            if (spriteIndex > spriteMax - 1) {
+            if(spriteIndex >spriteMax-1){
                 spriteIndex = 0;
             }
             spriteCounter = 0;
         }
     }
 
+
     @Override
     public void draw(Graphics2D g) {
-
-        g.drawImage(atk[spriteIndex], x, y, width, height, null);
+        g.drawImage(atk[spriteIndex],x,y,width,height,null);
     }
 
-    private void getAttackImgs(String spritePath) {
+    private void getAttackImgs(String spritePath){
         try {
 
             SpriteParser parser = SpriteParser.getInstance();
-            List<String> paths = parser.parseFileIntoSequence(spritePath);
+            List<String> paths  = parser.parseFileIntoSequence(spritePath);
             spriteMax = paths.size();
 
             atk = new BufferedImage[paths.size()];
-            int i = 0;
-            for (String s : paths) {
+            int i =0;
+            for(String s : paths){
                 atk[i] = ImageIO.read(getClass().getResourceAsStream(s));
                 i++;
             }
 
-            if (this.height == 0 && this.width == 0) {
-                this.width = atk[0].getWidth() / 8;
-                this.height = atk[0].getHeight() / 8;
-            }
 
-            System.out.println("WIDTH = " + width);
+            System.out.println("WIDTH = "+width);
 
         } catch (IOException e) {
 
         }
 
+
     }
+
 
 }
