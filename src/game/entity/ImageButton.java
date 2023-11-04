264d7525af6972bb3f8731dcd79460372b6227e6
Preetham Reddy Konuganti <pkonugan@asu.edu>
2023-11-03 22:46:33 -0700

    TG-48 added setBackgroundImage method, Removed Character class extension

--- src/game/entity/ImageButton.java
+++ src/game/entity/ImageButton.java
@@ -3,20 +3,29 @@ package game.entity;
 import game.GamePanel;
 import game.event.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class ImageButton extends Character implements ButtonInterface{
+public class ImageButton extends Entity implements ButtonInterface{
 
-    public ImageButton(GamePanel gp, KeyHandler kh, int x, int y) {
-        super(gp, kh, x, y);
-    }
 
-    public ImageButton(GamePanel gp, KeyHandler kh, int x, int y, int width, int height) {
-        super(gp, kh, x, y, width, height);
+    public ImageButton(GamePanel gp, KeyHandler kh,int x, int y,  int width, int height) {
+        super(gp, kh);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        atk = new BufferedImage[1];
     }
 
     public void setBackgroundImage(String file){
-
+        try {
+            atk[0] = ImageIO.read(getClass().getResourceAsStream(file));
+        } catch (IOException e) {
+            System.out.println("Could not set button image : "+e);
+        }
     }
 
     @Override
@@ -31,6 +40,26 @@ public class ImageButton extends Character implements ButtonInterface{
 
     @Override
     public void setEnable(boolean isEnabled) {
+
+    }
+
+    @Override
+    public void setBackgroundColor(Color color) {
+
+    }
+
+    @Override
+    public void setTextColor(Color color) {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.drawImage(atk[spriteIndex],x,y,width,height,null);
+    }
+
+    @Override
+    public void update() {
 
     }
 }
