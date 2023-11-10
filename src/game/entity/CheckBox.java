e97dbb4e232fcc2369b6d92ad3014feca5cfb425
lkumtamu <lkumtamu@asu.edu>
2023-11-09 22:08:20 -0700

    TG-56 added onClicked and toggleCheck events

--- src/game/entity/CheckBox.java
+++ src/game/entity/CheckBox.java
@@ -7,7 +7,9 @@ import java.awt.*;
 
 public class CheckBox extends Entity{
 
-    String value = "";
+    private String title = "";
+    private boolean isChecked = false;
+
     public CheckBox(GamePanel gp, KeyHandler kh, int x, int y, int width, int height) {
         super(gp, kh);
         this.x = x;
@@ -21,13 +23,34 @@ public class CheckBox extends Entity{
         // logic for box rendering
         g.setColor(Color.GREEN);
         g.drawRect(x, y, 48, 48);
-
         g.setColor(Color.white);
-        g.drawString(value,x+72, y);
+        g.fillRect(x+2,y+2,44,44); //checkbox inner white background
+        g.setColor(Color.white);
+        g.drawString(title,x+72, y);
+        if(isChecked){
+            g.setColor(Color.green);
+            g.fillRect(x+10,y+10,28,28); //checked button rendering
+        }
     }
 
     @Override
     public void update() {
 
     }
+
+    @Override
+    public void onClicked() {
+        toggleCheck();
+        super.onClicked();
+    }
+
+    private void toggleCheck() {
+        if(isChecked){
+            isChecked = false;
+        }
+        else {
+            isChecked = true;
+        }
+    }
+
 }
