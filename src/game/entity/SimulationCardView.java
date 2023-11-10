c097bcf0d00eac9c2e8e618a15ba763cd31a5e25
Pradyumn Kumar Upadhyay <pupadh22@asu.edu>
2023-11-09 21:45:17 -0700

    TG-59 Added Card attributes (cardTittle, description, createdBy, ratings)

--- src/game/entity/SimulationCardView.java
+++ src/game/entity/SimulationCardView.java
@@ -6,12 +6,35 @@ import game.event.KeyHandler;
 import java.awt.*;
 
 public class SimulationCardView extends Entity{
+
+    private String cardTitle;
+    private String description;
+    private String createdBy;
+
+    private int ratings; //star rating out of 5
+
     public SimulationCardView(GamePanel gp, KeyHandler kh, int x, int y, int width, int height) {
         super(gp, kh);
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    public void setCardTitle(String cardTitle){
+        this.cardTitle = cardTitle;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
     }
 
     @Override
@@ -21,6 +44,10 @@ public class SimulationCardView extends Entity{
 
         g.setColor(Color.white);
         g.fillRect(x+16, y+16,width-32, height-32);
+
+        g.setColor(Color.blue);
+        g.drawString(cardTitle,x+20,y+20);
+        g.drawString(description,x+18,y+44);
 
     }
 
