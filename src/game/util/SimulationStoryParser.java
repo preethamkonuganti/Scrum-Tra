4e3694e27ee3673b1ae9d829bbb21d76f36e4d78
lkumtamu <lkumtamu@asu.edu>
2023-11-17 21:31:53 -0700

    TG-62 Created SimulationStory Parser

--- src/game/util/SimulationStoryParser.java
+++ src/game/util/SimulationStoryParser.java
@@ -3,6 +3,7 @@ package game.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SimulationStoryParser {
@@ -16,26 +17,43 @@ public class SimulationStoryParser {
             instance = new SimulationStoryParser();
         return instance;
     }
-    public JSON parseFileIntoSequence(String fPath) {
+    public static List<String> parseSim(String fpath){
+        List<String> sim = getInstance().parseFileIntoSequence(fpath);
+        boolean isConvoOpen = false;
+        List<String> res = new ArrayList<>();
+        String by = "";
+        for(String s : sim){
+            if(s.startsWith("<convo")){
+                by = s.substring(10,s.length()-1);
+                isConvoOpen = true;
+                continue;
+            }
+            else if(s.startsWith("</convo>")){
+                isConvoOpen = false;
+                continue;
+            }
+            if(isConvoOpen){
+                res.add(by + " > "+s);
+            }
+        }
+        return res;
+    }
+
+    public List<String> parseFileIntoSequence(String fPath) {
         File file = new File(getClass().getResource(fPath).getFile());
-
-
         ArrayList<String> sequence = new ArrayList<>();
         Scanner sc = null;
-
-        String res = "";
-
         try {
             sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                res += line;
+                sequence.add(line.trim());
             }
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        return JSON.parseString(res);
+        return sequence;
     }
 }
