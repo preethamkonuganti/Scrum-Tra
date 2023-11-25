8e4216d9bfe2b99033f24a91b238a2f244b12a97
Pradyumn Kumar Upadhyay <pupadh22@asu.edu>
2023-11-24 23:28:49 -0700

    TG-85 Added Radio button class

--- src/game/entity/RadioGroup.java
+++ src/game/entity/RadioGroup.java
@@ -2,23 +2,24 @@ package game.entity;
 
 import game.GamePanel;
 import game.event.KeyHandler;
+import game.event.MouseClickInterface;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 
 /**
  * RadioGroup entity, provides radio buttons to select one answer among the given choices.
  */
 public class RadioGroup extends Entity{
 
-    private String title;
-    public RadioGroup(GamePanel gp, KeyHandler kh, int x, int y, int width, int height, String title) {
+    private ArrayList<CustomRadioButton> buttons;
+    public RadioGroup(GamePanel gp, KeyHandler kh, int x, int y) {
         super(gp, kh);
-        this.title = title;
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
-        this.title = title;
+        width = height = 0;
+        buttons = new ArrayList<CustomRadioButton>();
     }
 
     @Override
@@ -26,12 +27,34 @@ public class RadioGroup extends Entity{
         g.setColor(Color.decode("#5F259F"));
         g.fillRect(x, y, width, height);
 
-        g.setColor(Color.white);
-        g.fillRect(x+4, y+4,width-8, height-8);
+//        g.setColor(Color.white);
+//        g.fillRect(x+4, y+4,width-8, height-8);
+
+        buttons.forEach(new Consumer<CustomRadioButton>() {
+            @Override
+            public void accept(CustomRadioButton customRadioButton) {
+                customRadioButton.draw(g);
+            }
+        });
     }
 
     @Override
     public void update() {
 
+    }
+
+    public void addOption(String title){
+        CustomRadioButton radioButton = new CustomRadioButton(gp, kh, x, y+buttons.size()*60, title);
+        buttons.add(radioButton);
+        if(width < radioButton.getTitleWidth()){
+            width  = radioButton.getTitleWidth();
+        }
+        height += 60;
+        radioButton.setOnClickListener(new MouseClickInterface() {
+            @Override
+            public void onClicked() {
+                System.out.println();
+            }
+        });
     }
 }
