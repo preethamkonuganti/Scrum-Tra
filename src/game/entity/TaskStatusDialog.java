119bc9dfe0300c3e1c383552de47750ed36a529c
harimill <harimill@asu.edu>
2023-12-01 21:20:36 -0700

    TG-89

--- src/game/entity/TaskStatusDialog.java
+++ src/game/entity/TaskStatusDialog.java
@@ -1,6 +1,7 @@
 package game.entity;
 
 import game.GamePanel;
+import game.data.ScrumTask;
 import game.event.KeyHandler;
 import game.event.MouseClickInterface;
 
@@ -10,11 +11,9 @@ import java.awt.*;
  * TaskStatusDialog , ui entity to select task status
  * enables user to change the task status
  */
-public class TaskStatusDialog extends Entity {
+public class TaskStatusDialog extends Entity{
 
-    private String title;
-    private int taskId;
-    private int assignedTo;
+    private ScrumTask task;
     private ImageButton closeBtn;
 
     private RadioGroup taskStatusGroup;
@@ -28,10 +27,11 @@ public class TaskStatusDialog extends Entity {
         this.width = 300;
         this.height = 380;
 
-        closeBtn = new ImageButton(gp, kh, x + 220, y + 10, 40, 40);
+
+        closeBtn = new ImageButton(gp,kh,x+220,y+10,40,40);
         closeBtn.setBackgroundImage("/assets/back.png");
 
-        taskStatusGroup = new RadioGroup(gp, kh, x + 20, y + 100);
+        taskStatusGroup = new RadioGroup(gp,kh,x+20,y+100);
 
         taskStatusGroup.addOption(ScrumTaskDetailsDialog.TASK_STATUS.NEW.toString());
         taskStatusGroup.addOption(ScrumTaskDetailsDialog.TASK_STATUS.IN_PROGRESS.toString());
@@ -46,10 +46,19 @@ public class TaskStatusDialog extends Entity {
         });
     }
 
-    public void setScrumTaskDetails(String title, int taskId, int assignedTo) {
-        this.title = title;
-        this.taskId = taskId;
-        this.assignedTo = assignedTo;
+    public void setScrumTask(ScrumTask task){
+        this.task = task;
+        switch (task.getTaskStatus()){
+            case NEW -> {
+                taskStatusGroup.setSelected(0);
+            }
+            case IN_PROGRESS -> {taskStatusGroup.setSelected(1);
+               }
+            case TEST_READY -> {taskStatusGroup.setSelected(2);
+               }
+            case DONE -> {taskStatusGroup.setSelected(3);
+                }
+        }
     }
 
     @Override
@@ -58,29 +67,48 @@ public class TaskStatusDialog extends Entity {
         g.fillRect(x, y, width, height);
 
         g.setColor(Color.white);
-        g.fillRect(x + 4, y + 4, width - 8, height - 8);
+        g.fillRect(x+4, y+4,width-8, height-8);
 
         g.setFont(new Font("Serif", Font.ITALIC, 20));
         g.setColor(Color.decode("#5F259F"));
-        g.drawString(title, x + 20, y + 24);
+        g.drawString(task.getTitle(),x+20,y+24);
 
-        g.drawString("Task status ", x + 20, y + 64);
+        g.drawString("Task status ",x+20,y+64);
+
 
         closeBtn.draw(g);
 
         taskStatusGroup.draw(g);
     }
 
+
     @Override
     public void update() {
 
     }
 
-    public String getTaskStatus() {
-        return taskStatusGroup.getSelected();
+    public ScrumTaskDetailsDialog.TASK_STATUS getTaskStatus(){
+        switch (taskStatusGroup.getSelectedIndex()){
+            case 0: return ScrumTaskDetailsDialog.TASK_STATUS.NEW;
+            case 1: return ScrumTaskDetailsDialog.TASK_STATUS.IN_PROGRESS;
+            case 2: return ScrumTaskDetailsDialog.TASK_STATUS.TEST_READY;
+            case 3: return ScrumTaskDetailsDialog.TASK_STATUS.DONE;
+            default:return ScrumTaskDetailsDialog.TASK_STATUS.BLOCKER;
+        }
+
     }
 
     public void setScreenListener(ScrumTaskDetailsDialog.ScreenListener screenListener) {
         this.screenListener = screenListener;
+    }
+
+    public void removeObservers(){
+        closeBtn.pauseObserver();
+        taskStatusGroup.pauseObserver();
+        pauseObserver();
+    }
+
+    public void setTaskStatus(ScrumTaskDetailsDialog.TASK_STATUS status) {
+        task.setTaskStatus(status);
     }
 }
